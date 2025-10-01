5a74a191bed475a150727cead59139b398e2d13b
ezeguzman26 <ezegtalleres14@gmail.com>
2025-10-01 14:27:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 console.log("Landing Sovereign & Co. cargada correctamente");
 // WhatsApp flotante ya no requiere script adicional
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+// Cerrar menú al hacer click en un link
+document.querySelectorAll('.nav-menu a').forEach(link => {
+  link.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  });
+});
+const closeMenu = document.getElementById('close-menu');
+
+closeMenu.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+});
